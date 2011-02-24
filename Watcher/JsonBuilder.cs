d60daa40d99d0606d66c277f5dc4fc6d7a7fbb7e
Herberth Amaral <herberthamaral@gmail.com>
2011-02-24 08:56:22 -0300

    Adding DeskMetrics info into some files

--- Watcher/JsonBuilder.cs
+++ Watcher/JsonBuilder.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿// **********************************************************************//
+//                                                                       //
+//     DeskMetrics NET - DeskMetricsOS.cs                                //
+//     Copyright (c) 2010-2011 DeskMetrics Limited                       //
+//                                                                       //
+//     http://deskmetrics.com                                            //
+//     http://support.deskmetrics.com                                    //
+//                                                                       //
+//     support@deskmetrics.com                                           //
+//                                                                       //
+//     This code is provided under the DeskMetrics Modified BSD License  //
+//     A copy of this license has been distributed in a file called      //
+//     LICENSE with this source code.                                    //
+//                                                                       //
+// **********************************************************************//
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
