e584f4916ef5b99aeea2164ba06477a2b0799b14
Herberth Amaral <herberthamaral@gmail.com>
2011-04-05 16:25:39 -0300

    Fixing some path issues

--- DeskMetricsConsole/Main.cs
+++ DeskMetricsConsole/Main.cs
@@ -6,35 +6,33 @@ namespace DeskMetricsConsole
 	class MainClass
 	{
 		public static void Main (string[] args)
+		{		
+			if (args.Length != 3)
+			{
+				PrintUsage();
+				return;
+			}
+			
+			
+			var watcher = new Watcher();
+			if (args[0] == "Install")
+			{
+				watcher.TrackInstall(args[2],args[1]);
+			}
+			
+			if (args[0] == "Uninstall")
+			{
+				watcher.TrackUninstall(args[2],args[1]);
+			}
+		}
+		
+		public static void PrintUsage()
 		{
-			var watcher = new DeskMetrics.Watcher();
-			Console.Write("Starting...");
-			watcher.Start("4d47c012d9340b116a000000","0.1");
-			Console.WriteLine("[ok]");
-			
-			Console.Write("Adding event...");
-			watcher.TrackEvent("DeskMetricsConsole","InitialEvent");
-			Console.WriteLine("[ok]");
-			
-			Console.Write("Adding event value...");
-			watcher.TrackEventValue("DeskMetricsConsole","EventValue","Aha!");
-			Console.WriteLine("[ok]");
-			
-			Console.Write("Adding event timed...");
-			watcher.TrackEventPeriod("DeskMetricsConsole","EventValue",10,true);
-			Console.WriteLine("[ok]");
-			
-			Console.Write("Adding custom data...");
-			watcher.TrackCustomData("DeskMetricsConsole","CustomData");
-			Console.WriteLine("[ok]");
-			
-			Console.Write("Adding custom data in real time...");
-			watcher.TrackCustomDataR("DeskMetricsConsole","MyCustomDataInRealTime");
-			Console.WriteLine("[ok]");
-			
-			Console.Write("Finishing app and sending data to DeskMetrics");
-			watcher.Stop();
-			Console.ReadLine();
+			Console.WriteLine("Usage: deskmetrics [operation] app_id app_version");
+			Console.WriteLine("Where: ");
+			Console.WriteLine("   operation: One of supported operations (Install, Uninstall)");
+			Console.WriteLine("   app_id: Your application ID (like 4d47d012d9340b116a000000)");
+			Console.WriteLine("   app_version: Your application version (like 4.2)");
 		}
 	}
 }
